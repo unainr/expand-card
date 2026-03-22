66c0745ea74a470bda725a33f3d6f1ffb1ea247b
UNAINR <munain605@gmail.com>
2026-03-22 18:29:43 +0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import ExpandOnHover from "@/components/ui/expand-cards";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen">
+      <ExpandOnHover />
+    </div>
+  );
+}
